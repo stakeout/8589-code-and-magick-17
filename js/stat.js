22a6a592fd6112f0278443f9c78ca37253a90bbf
Vadim Novash <novash@tut.by>
2019-06-09 16:17:12 +0300

    diagram done

--- js/stat.js
+++ js/stat.js
@@ -13,11 +13,12 @@ var TEXT_RESULTS_TITLE = 'Список результатов:';
 var HISTOGRAM_HEIGHT = 150;
 var HISTOGRAM_COLUMN_WIDTH = 40;
 var HISTOGRAM_COLUMN_OFFSET = 50;
+var USER_BG_COLOR = 'rgba(0, 0, 255, 1)';
 
 var renderCloud = function (ctx, color, x, y, width, height) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
-}
+};
 
 var renderText = function (ctx, color, font, textBaseline, text, x, y) {
   ctx.fillStyle = color;
@@ -26,9 +27,38 @@ var renderText = function (ctx, color, font, textBaseline, text, x, y) {
   ctx.fillText(text, x, y);
 };
 
+var getBlueShade = function () {
+  return 'rgba(0, 0, 255, ' + Math.random().toFixed(2) + ')';
+};
+
+var getMaxElementOfArray = function (array) {
+  var maxElement = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (maxElement < array[i]) {
+      maxElement = array[i];
+    }
+  }
+  return Math.floor(maxElement);
+};
+
+var renderBar = function (ctx, color, name, time, index, barHeight, x) {
+  var positionX = x + HISTOGRAM_COLUMN_OFFSET + (HISTOGRAM_COLUMN_WIDTH + HISTOGRAM_COLUMN_OFFSET) * index;
+  ctx.fillStyle = color;
+  ctx.fillRect(positionX, CLOUD_HEIGHT - barHeight - STEP * 4, HISTOGRAM_COLUMN_WIDTH, barHeight);
+  ctx.fillStyle = CLOUD_SHADOW_COLOR;
+  ctx.fillText(name, positionX, CLOUD_HEIGHT - STEP * 2);
+  ctx.fillText(time, positionX, CLOUD_HEIGHT - barHeight - STEP * 8);
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_SHADOW_COLOR, CLOUD_POSITION_X + STEP * 2, CLOUD_POSITION_Y + STEP * 2, CLOUD_WIDTH, CLOUD_HEIGHT);
   renderCloud(ctx, CLOUD_COLOR, CLOUD_POSITION_X, CLOUD_POSITION_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
   renderText(ctx, CLOUD_SHADOW_COLOR, '16px PT Mono', 'hanging', TEXT, CLOUD_POSITION_X + STEP * 4, CLOUD_POSITION_Y + STEP * 4);
   renderText(ctx, CLOUD_SHADOW_COLOR, '16px PT Mono', 'hanging', TEXT_RESULTS_TITLE, CLOUD_POSITION_X + STEP * 4, CLOUD_POSITION_Y + STEP * 4 + SPACE_BETWEEN_TEXT_LINES);
-}
+  var maxPlayerTime = getMaxElementOfArray(times);
+  for (var i = 0; i < names.length; i++) {
+    var barHeight = HISTOGRAM_HEIGHT * times[i] / maxPlayerTime;
+    var barColor = names[i] === 'Вы' ? USER_BG_COLOR : getBlueShade();
+    renderBar(ctx, barColor, names[i], Math.floor(times[i]), i, barHeight, CLOUD_POSITION_X);
+  }
+};
